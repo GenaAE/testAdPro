9194aad0cd3556ed9f15e848e82d4149ea86404b
EugeneAE <eremin.ea@gmail.com>
2023-08-26 11:43:33 +0300

    create comp, axios

---
+++ test/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Привет</h1>
+    <button @click="fetchProjects" type="button">Give me projects</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  methods: {
+    async fetchProjects() {
+      try {
+        const res = await axios.get(
+          'https://dev.aicap.tech/api/v1/interview/projects/'
+        );
+        console.log(res);
+      } catch (err) {
+        alert('problems');
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
